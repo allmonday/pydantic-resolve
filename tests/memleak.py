2e3f50e75ea85997fd05d5632484d71dfe5f659b
tangkikodo <allmonday@126.com>
2025-10-13 22:23:19 +0800

    fix mem leak from contextvars

--- tests/memleak.py
+++ tests/memleak.py
@@ -1,3 +1,4 @@
+import contextvars
 import asyncio
 import tracemalloc
 from pydantic import BaseModel
@@ -6,6 +7,14 @@ from pydantic_resolve import Resolver
 class Model(BaseModel):
     id: int
 
+class Demo:
+    def __init__(self):
+        self.vars = contextvars.ContextVar("demo_var")
+
+    def do(self):
+        token = self.vars.set([1,2,3,4,5,6,7,8,9,10])
+        self.vars.reset(token)
+    
 
 async def test_memleak():
     tracemalloc.start()
@@ -13,6 +22,7 @@ async def test_memleak():
 
     for _ in range(10000):
         await Resolver().resolve(Model(id=1))
+        # Demo().do()
 
     snapshot2 = tracemalloc.take_snapshot()
     top_stats = snapshot2.compare_to(snapshot1, 'lineno')
